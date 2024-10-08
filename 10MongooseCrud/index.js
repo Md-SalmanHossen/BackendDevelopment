95372e38c1b304cc7e9ebfb6e870b991042f300c
SalmanHossen <hossainsalman873@gmail.com>
2024-10-08 16:08:39 +0600

    basic crud in localhost from lwt

--- 10MongooseCrud/index.js
+++ 10MongooseCrud/index.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+
+const express=require('express');
+const mongoose=require('mongoose');
+const todoHandler=require('./routes/todoHandler');
+
+
+const app=express();
+app.use(express.json());
+
+//database connection with mongoose
+mongoose
+    .connect('mongodb://localhost/todos')
+    .then(()=>console.log('connecting successfully '))
+    .catch(err=>console.log('Database connection error:',err));
+
+
+//application routes
+app.use('/todo',todoHandler)
+
+
+//default error handler
+function errorHandler(err,req,res,next){
+    if(res.headerSent){
+        return next(err);
+    }
+    res.status(500).json({err:err.message});
+}
+
+
+
+app.listen(3000,()=>{
+    console.log('app listening at port 3000');
+})
