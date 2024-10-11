1b331200ad131f2b5f5a8bf6c81510af938ae355
SalmanHossen <hossainsalman873@gmail.com>
2024-10-11 22:12:35 +0600

    remove the app.js file

--- 8Authen_Authorization/index.js
+++ 8Authen_Authorization/index.js
@@ -7,6 +7,10 @@ const app=express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/',(req,res)=>{
+    res.send('the port is running');
+})
+
 app.listen(3000,()=>{
     console.log(`the port is running 3000`);
 });
