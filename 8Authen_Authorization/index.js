3e8d6da1ecbec7244c267e6f2a6bfe90ff7a1aed
SalmanHossen <hossainsalman873@gmail.com>
2024-10-11 22:56:11 +0600

    data encrypt and decrypt

--- 8Authen_Authorization/index.js
+++ 8Authen_Authorization/index.js
@@ -1,5 +1,6 @@
 
 const cookieParser=require('cookie-parser');
+const bcrypt=require('bcrypt')
 const express=require('express');
 const cors=require('cors');
 const app=express();
@@ -10,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+/*
 //cookie set and read.
-
 //route in get
 app.get('/',(req,res)=>{
     // res.send('the port is running');
@@ -22,6 +23,32 @@ app.get('/',(req,res)=>{
 app.get('/read',(req,res)=>{
     console.log(req.cookies)
     res.send("read page");
+})
+
+*/
+//$2b$10$OHH7hhgp68/idRTjL6oLUenvlswuPe/u0568opI3Mo9QoyGl1ioD6
+
+//
+app.get('/',(req,res)=>{
+    res.send('working');
+})
+
+
+//data encrypt
+app.get('/read',(req,res)=>{
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash('abcdefg',salt,(err,has)=>{
+            console.log(has)
+        })
+    })
+});
+
+//data decryption
+app.get('/decrypt',(req,res)=>{
+    bcrypt.compare('abcdefg','$2b$10$OHH7hhgp68/idRTjL6oLUenvlswuPe/u0568opI3Mo9QoyGl1ioD6',(err,result)=>{
+        console.log(result);
+        res.send('running')
+    })
 })
 
 
