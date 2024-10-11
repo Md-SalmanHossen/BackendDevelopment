04bd2a9f25c6779004d14ca47a87f4507f91eef8
SalmanHossen <hossainsalman873@gmail.com>
2024-10-11 22:29:06 +0600

    cookie set and read

--- 8Authen_Authorization/index.js
+++ 8Authen_Authorization/index.js
@@ -1,16 +1,31 @@
 
-
+const cookieParser=require('cookie-parser');
 const express=require('express');
 const cors=require('cors');
 const app=express();
 
+
+//middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
+//cookie set and read.
+
+//route in get
 app.get('/',(req,res)=>{
-    res.send('the port is running');
+    // res.send('the port is running');
+    res.cookie("name","salman");
+    res.send('done')
+});
+
+app.get('/read',(req,res)=>{
+    console.log(req.cookies)
+    res.send("read page");
 })
 
+
+//running port
 app.listen(3000,()=>{
     console.log(`the port is running 3000`);
 });
